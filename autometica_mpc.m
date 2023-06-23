02fbf00f4d12e443830eb981721765f8db94595d
kjsoo0146 <110090496+kjsoo0146@users.noreply.github.com>
2023-06-23 18:20:33 +0900

    .

--- autometica_mpc.m
+++ autometica_mpc.m
@@ -1,4 +1,4 @@
-function [TM, u, t] = autometica_mpc(TM, Nl, x)
+function [nTM, u, t] = autometica_mpc(TM, Nl, x)
 
     coder.extrinsic("optimconstr", "optimproblem", "optimvar");
 %==========================================================================
@@ -37,8 +37,6 @@ function [TM, u, t] = autometica_mpc(TM, Nl, x)
     constT = optimconstr(30,Tset+1);
        
     LL = param.tildeF - param.tildeG*param.tildeK;
-    PHI = (param.tildeA - param.tildeB*param.tildeK);
-    PSI = []
     for i= 1:Tset
         constT(:,i) = LL*(param.phi^(i))*x(:,M+Nl)<=[1; 1;1; 1;1; 1;];
     end
@@ -48,6 +46,7 @@ function [TM, u, t] = autometica_mpc(TM, Nl, x)
 % triggering constraints
     SizeOfTM = size(TM);
     tm = TM(2:SizeOfTM(2));
+    SizeOftm = size(tm)
     tmcomp = find(tm);
     SizeOfTmcomp = size(tmcomp);
     triggering_constr = optimconstr(param.N);
@@ -62,8 +61,59 @@ function [TM, u, t] = autometica_mpc(TM, Nl, x)
             triggering_constr(tmcomp(i)) =  u(:,tmcomp(i)-1) ~= u(:,tmcomp(i));
         end
     end
-    nontmcomp = find(~tm);
+    nontmcomp = find(~tm); 
+    SizeOfnontmcomp = size(nontmcomp);
 %==========================================================================
 % implementing QP
+%--------------------------------------------------------------------------
+% +0
+    original_tm = tm;
+    for a= 0:SizeOfnontmcomp %% 트리거 횟수 추가
+        for b = 0:a 
+            fvalArr = [];
+            solArr = [];
+            tmarr = []
+            ttime =a;
+            % while(1)
+            %      이전 스텝에서 받은 required trigger time moment sequence에서 trigger일때를 1 nontrigger 일때를 0으로 하여 벡터로 만들었다.
+            %      이때 QP가 feasible 해질 때까지 trigger time을 추가하면서 QP를 풀어야 하는데, 즉 a =0,1,.... time sequence에서 a개의 nontrigger time을 뽑아 trigger time으로 변환해서 
+            %      QP를 풀어함...
+            % end
+            
+        end
+    end
+    % prob.Constraints.triggering_constr = triggering_constr;
+    % [sol,fval,exitflag,output,lambda] = solve(prob);
+    % if exitflag == 1
+    %     u = sol;
+    %     t = tmcomp(1);
+    %     nTM = tm(tmcomp(1)+1:SizeOftm)
 end
     
+
+% fevalArr = [];
+%             
+%             for c = 0:SizeOfnontmcomp %% 트리거 어디다 놓을지
+%                 plustime = a
+%                 while(1)
+%                     tm(nontmcomp(c)) = 1;
+                    
+%                 for i = 1:SizeOfTmcomp %% required triggering time moment setting
+%                     if tmcomp(i)==1
+%                         triggering_constr(tmcopm(i)) = u0 ~= u(:,1);
+%                     else
+%                         triggering_constr(tmcomp(i)) =  u(:,tmcomp(i)-1) ~= u(:,tmcomp(i));
+%                     end
+%                 end
+%                 [sol, fval, exitflag, output, lambda] = solve(prob);
+               
+%                 fevalArr = [feasible exitflag];
+%                 fvalArr = [fvalArr, fval];
+%                 solArr = [solArr, sol];
+%             end
+%             feasible = find(fevalArr,1);
+%             if isempty(feasible) == 0
+%                [row col]= min(fvalArr);
+%                utemp = solarr(col);
+%                u = utemp.u0;
+%                ttemp = 
