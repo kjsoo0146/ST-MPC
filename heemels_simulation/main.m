1aeb10ae6554f296d712a1d65d3dc9d4de3c5bdb
kjsoo0146 <kjsoo0146@gmail.com>
2023-12-09 21:55:16 +0900

    .

--- heemels_simulation/main.m
+++ heemels_simulation/main.m
@@ -1,11 +1,12 @@
 clc;
 clear all;
-close all;
 
 param = param_setup();
 param.heemesl_Tset = Tset_d(param);
 save('param', 'param');
 
 out = sim('self_triggered_mpc_simulation.slx');
-
+figure(1)
 plot(out.state)
+figure(2)
+plot(out.input)
